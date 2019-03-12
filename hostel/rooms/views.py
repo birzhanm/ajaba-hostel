5d09bcf5a5dec3e11a200a911c17c0e27be541b9
birzhanm <birzhanm@gmail.com>
2019-03-12 21:10:07 +0600

    added detail view for blocks

--- hostel/rooms/views.py
+++ hostel/rooms/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Block
 
 # Create your views here.
 def block_list(request):
     blocks = Block.objects.all()
     context = {'blocks':blocks}
-    return render(request, 'rooms/block_list.html',context)
+    return render(request, 'rooms/block_list.html', context)
+
+def block_detail(request, pk):
+    current_block = get_object_or_404(Block, pk=pk)
+    rooms = current_block.room_set.all()
+    context = {'current_block': current_block, 'rooms':rooms}
+    return render(request, 'rooms/block_detail.html', context)
